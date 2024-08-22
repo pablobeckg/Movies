7b27115f7822d9ebbfd1dfbc7275e15de809dc10
pablobeckg <beckgarreaud@gmail.com>
2024-08-22 16:59:25 +0200

    added user

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,25 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import Header from "./components/Header/Header";
+import LoginPage from "./pages/Login/LoginPage";
+import LoginStatus from "./components/LoginStatus/LoginStatus";
+import { UserProvider } from "./context/UserContext";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Header></Header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <UserProvider>
+        <BrowserRouter>
+          <LoginStatus />
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<LoginPage />} />
+            </Routes>
+          </main>
+        </BrowserRouter>
+      </UserProvider>
     </>
   );
 }
---
+++ src/components/LoginStatus/LoginStatus.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import "./LoginStatus.css";
+import { useUserContext } from "../../context/UserContext";
+import supabaseClient from "../../lib/supabaseClient";
+
+const LoginStatus = () => {
+  const userContext = useUserContext();
+  const user = userContext?.user;
+
+  const handleLogoutClicked = async (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    const signoutResponse = await supabaseClient.auth.signOut();
+
+    if (signoutResponse.error) {
+        console.error('Logout error', signoutResponse.error)
+    }else {
+        userContext?.setUser(null);
+    }
+  }
+
+  return (
+    <div className="login-status">
+      {user ? (
+        <div>
+          <span>Welcome, {user.email}</span>
+          <button className="logout-button" onClick={(handleLogoutClicked)}>Logout</button>
+        </div>
+      ) : (
+        <button>
+          <Link to="/login">Login</Link>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default LoginStatus;
